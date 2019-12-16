b525b0af151adf8769343664a60847d4ec7a4a21
Max <maxtegnebo@hotmail.com>
2019-12-16 23:44:44 +0100

    Job Expenses

---
+++ Job_Expenses/Job_Expenses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Job_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nrOfValues = int.Parse(Console.ReadLine());
+            if (nrOfValues == 0)
+            {
+                Console.WriteLine(0);
+            }
+            else
+            {
+                string input = Console.ReadLine();
+                string[] values = input.Split(' ');
+
+                int sum = 0;
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    int value = int.Parse(values[i]);
+                    if (value < 0)
+                    {
+                        sum = sum + value;
+                    }
+                }
+                Console.WriteLine(sum / -1);
+            }
+            Console.ReadLine();
+        }
+    }
+}
